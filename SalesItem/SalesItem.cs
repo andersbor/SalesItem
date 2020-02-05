02fb080313c596f7c0a23a7adfacd37d6a8c34df
Anders Børjesson <anbo@zealand.dk>
2020-02-05 20:03:50 +0100

    Add project files.

---
+++ SalesItem/SalesItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SalesItem
+{
+    public class SalesItem
+    {
+        private string _name;
+        private double _price;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value == null) throw new ArgumentNullException();
+                if (value.Length < 2) throw new ArgumentException();
+                _name = value;
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0.0) throw new ArgumentOutOfRangeException();
+                _price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Id + " " + Name + " " + Price;
+        }
+    }
+}
---
+++ SalesItemTests/SalesItemTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalesItem.Tests
+{
+    [TestClass]
+    public class SalesItemTests
+    {
+        private readonly SalesItem _item = new SalesItem();
+
+        [TestMethod]
+        public void NormalTest()
+        {
+            _item.Name = "ab";
+            Assert.AreEqual("ab", _item.Name);
+            _item.Price = 14.95;
+            Assert.AreEqual(14.95, _item.Price);
+
+            _item.Price = Math.PI;
+            Assert.AreEqual(3.14159265, _item.Price, 0.001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NameNullTest()
+        {
+            _item.Name = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NameShortTest()
+        {
+            _item.Name = "a";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PriceNegativeTest()
+        {
+            _item.Price = -0.01;
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            SalesItem item = new SalesItem {Id = 1, Name = "pencil", Price = 4};
+            Assert.AreEqual("1 pencil 4", item.ToString());
+        }
+    }
+}
